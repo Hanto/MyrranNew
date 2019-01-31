d9d8a35f56314226e8f5f83ddbb2242daa50ba7f
Hanto <jhanto@gmail.com>
2019-01-31 10:14:04 +0100

    v0.121

--- core/src/main/java/com/myrran/view/ui/customspell/CustomDebuffIconView.java
+++ core/src/main/java/com/myrran/view/ui/customspell/CustomDebuffIconView.java
@@ -1,8 +1,6 @@
 package com.myrran.view.ui.customspell;
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Disposable;
 import com.myrran.controller.CustomSpellController;
 import com.myrran.controller.DadDebuffTarget;
@@ -10,31 +8,17 @@ import com.myrran.model.spell.generators.CustomDebuffSlot;
 import com.myrran.model.spell.generators.CustomSpellDebuff;
 import com.myrran.view.ui.Atlas;
 import com.myrran.view.ui.listeners.TouchDownRightListener;
-import com.myrran.view.ui.widgets.WidgetGroup;
-import com.myrran.view.ui.widgets.WidgetImage;
-import com.myrran.view.ui.widgets.WidgetText;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 /** @author Ivan Delgado Huerta */
-public class CustomDebuffIconView extends WidgetGroup implements PropertyChangeListener, Disposable
+public class CustomDebuffIconView extends CustomIconView implements PropertyChangeListener, Disposable
 {
     private CustomDebuffSlot modelSlot;
     private CustomSpellDebuff modelDebuff;
     private CustomSpellController controller;
-
-    private WidgetGroup icon;
-    private WidgetImage debuffIcon;
-    private WidgetText cost;
-    private WidgetText debuffName;
-    private WidgetText slotType;
-    private WidgetText lock;
     private DadDebuffTarget dadTarget;
-
-    private static final BitmapFont font10 = Atlas.get().getFont("10");
-    private static final BitmapFont font14 = Atlas.get().getFont("14");
-    private static final Color magenta = new Color(170/255f, 70/255f, 255/255f, 1f);
 
     // CONSTRUCTOR:
     //--------------------------------------------------------------------------------------------------------
@@ -42,19 +26,11 @@ public class CustomDebuffIconView extends WidgetGroup implements PropertyChangeL
     public CustomDebuffIconView(CustomSpellController customSpellController)
     {
         controller  = customSpellController;
-        icon        = new WidgetGroup();
-        debuffIcon  = new WidgetImage();
-        cost        = new WidgetText(font10, magenta, Color.BLACK, 1);
-        debuffName  = new WidgetText(font10, Color.ORANGE, Color.BLACK, 1);
-        slotType    = new WidgetText(font10, Color.WHITE, Color.BLACK, 1);
-        lock        = new WidgetText(font10, Color.WHITE, Color.BLACK, 1);
         dadTarget   = new DadDebuffTarget(this, controller);
 
         controller.getDadDebuff().addTarget(dadTarget);
         addListener(new TouchDownRightListener(event ->
             controller.removeCustomSpellDebuff(modelSlot)));
-
-        createLayout();
     }
 
     @Override public void dispose()
@@ -80,9 +56,10 @@ public class CustomDebuffIconView extends WidgetGroup implements PropertyChangeL
         disposeObservers();
 
         if (customDebuffSlot == null)
-            removeModel();
+            setVisible(false);
         else
         {
+            setVisible(true);
             modelSlot = customDebuffSlot;
             modelDebuff = modelSlot.getCustomSpellDebuff();
             dadTarget.setModel(modelSlot);
@@ -92,65 +69,31 @@ public class CustomDebuffIconView extends WidgetGroup implements PropertyChangeL
         }
     }
 
-    private void removeModel()
-    {
-        modelSlot = null;
-        debuffIcon.setTexureRegion((Atlas.get().getTexture("TexturasIconos/IconoVacio2")));
-        cost.setText(null);
-        debuffName.setText(null);
-        slotType.setText(null);
-        lock.setText(null);
-    }
-
     private void update()
     {
-        slotType.setText(modelSlot.getSlotType());
-        lock.setText(modelSlot.getLock().toString());
+        setName2(modelSlot.getSlotType());
 
         if (modelSlot.hasData())
         {
-            debuffIcon.setTexureRegion((Atlas.get().getTexture("TexturasIconos/FireBall2")));
-            cost.setText(modelDebuff.getTotalCost().toString());
-            debuffName.setText(modelDebuff.getName());
-            debuffName.setTextColor(Color.ORANGE);
+            setBackground(Atlas.get().getTexture("TexturasIconos/FireBall2"));
+            setCorner(modelDebuff.getTotalCost().toString());
+            setName1(modelDebuff.getName());
+            setName1Color(Color.ORANGE);
         }
         else
         {
-            debuffIcon.setTexureRegion((Atlas.get().getTexture("TexturasIconos/IconoVacio2")));
-            cost.setText(null);
-            debuffName.setText(modelSlot.getLock().toString().toLowerCase());
-            debuffName.setTextColor(Color.LIGHT_GRAY);
+            setBackground(Atlas.get().getTexture("TexturasIconos/IconoVacio2"));
+            setCorner(null);
+            setName1(modelSlot.getLock().toString().toLowerCase());
+            setName1Color(Color.LIGHT_GRAY);
         }
     }
 
-    // CREATE LAYOUT:
-    //--------------------------------------------------------------------------------------------------------
-
-    private void createLayout()
-    {
-        int vPad = -4;
-
-        Table textTable = new Table().bottom().left();
-        textTable.setWidth(64-5);
-        textTable.add(debuffName).left()  .padTop(vPad).padBottom(vPad).row();
-        textTable.add(slotType).left()    .padTop(vPad).padBottom(vPad+1);
-        textTable.add(cost).expand().fillX().right().padTop(vPad).padBottom(vPad-6).padRight(2);
-        //textTable.add(lock).left()        .padTop(-2).padBottom(vPad).width(50).row();
-
-        addActor(debuffIcon);
-        addActor(textTable);
-        textTable.setPosition(5, 8);
-
-        /*top().left();
-        add(icon).top().left();
-        add(textTable).top().left();*/
-    }
-
     public void setLockColor(Color color)
-    {   debuffName.setTextColor(color); }
+    {   setName1Color(color); }
 
     public void setDefaultLocColor()
-    {   debuffName.setTextColor(modelDebuff.hasData() ? Color.ORANGE : Color.LIGHT_GRAY); }
+    {   setName1Color(modelDebuff.hasData() ? Color.ORANGE : Color.LIGHT_GRAY); }
 
     // MVC:
     //--------------------------------------------------------------------------------------------------------
---
+++ core/src/main/java/com/myrran/view/ui/customspell/CustomIconView.java
@@ -0,0 +1,76 @@
+package com.myrran.view.ui.customspell;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.myrran.view.ui.Atlas;
+import com.myrran.view.ui.widgets.WidgetGroup;
+import com.myrran.view.ui.widgets.WidgetImage;
+import com.myrran.view.ui.widgets.WidgetText;
+
+/** @author Ivan Delgado Huerta */
+public class CustomIconView extends WidgetGroup
+{
+    private WidgetImage background;
+    private WidgetText name1;
+    private WidgetText name2;
+    private WidgetText corner;
+
+    private static final BitmapFont font10 = Atlas.get().getFont("10");
+    private static final Color magenta = new Color(170/255f, 70/255f, 255/255f, 1f);
+
+    // CONSTRUCTOR:
+    //--------------------------------------------------------------------------------------------------------
+
+    public CustomIconView()
+    {
+        background  = new WidgetImage();
+        name1       = new WidgetText(font10, Color.ORANGE, Color.BLACK, 1);
+        name2       = new WidgetText(font10, Color.WHITE, Color.BLACK, 1);
+        corner      = new WidgetText(font10, magenta, Color.BLACK, 1);
+
+        createLayout();
+    }
+
+    // SETTERS - GETTERS:
+    //--------------------------------------------------------------------------------------------------------
+
+    public CustomIconView setBackground(TextureRegion texture)
+    {   background.setTexureRegion(texture); return this; }
+
+    public CustomIconView setName1(String name)
+    {   name1.setText(name); return this; }
+
+    public CustomIconView setName2(String name)
+    {   name2.setText(name); return this; }
+
+    public CustomIconView setCorner(String name)
+    {   corner.setText(name); return this; }
+
+    public CustomIconView setName1Color(Color color)
+    {   name1.setTextColor(color); return this; }
+
+    public CustomIconView setName2Color(Color color)
+    {   name2.setTextColor(color); return this; }
+
+    public CustomIconView setCornerColor(Color color)
+    {   corner.setTextColor(color); return this; }
+
+    // MAIN:
+    //--------------------------------------------------------------------------------------------------------
+
+    private void createLayout()
+    {
+        int pad = -4;
+
+        Table table = new Table().bottom().left().padLeft(5).padBottom(8);
+        table.setWidth(64);
+        table.add(name1).left().padTop(pad).padBottom(pad).row();
+        table.add(name2).left().padTop(pad).padBottom(pad+1);
+        table.add(corner).expand().fillX().right().padTop(pad).padBottom(pad-6).padRight(2);
+
+        addActor(background);
+        addActor(table);
+    }
+}
